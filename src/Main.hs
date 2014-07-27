9a35e410c997f93de5eac5181d4f3c4fe44a4848
Schell Scivally <efsubenovex@gmail.com>
2014-07-27 13:25:20 -0700

    Ditched daemon mode - fixes #5

--- src/Main.hs
+++ src/Main.hs
@@ -2,8 +2,7 @@
 module Main where
 
 import System.Environment        (getArgs)
-import System.Console.GetOpt 
-import System.Posix.Daemon
+import System.Console.GetOpt
 import Data.List
 import Control.Monad
 import Filesystem.Path.CurrentOS hiding (concat, null)
@@ -23,8 +22,6 @@ data Options = Options { optShowVersion :: Bool
                        , optCommands    :: [String]
                        , optPatterns    :: [String]
                        , optDirectory   :: FilePath
-                       , optIsDaemon    :: Bool
-                       , optDaemonCmd   :: String
                        } deriving (Show, Eq)
 
 defaultOptions :: Options
@@ -32,8 +29,6 @@ defaultOptions = Options { optShowVersion = False
                          , optCommands    = []
                          , optPatterns    = []
                          , optDirectory   = fromText $ T.pack "."
-                         , optIsDaemon    = False
-                         , optDaemonCmd   = "start"  
                          }
 
 options :: [OptDescr (Options -> Options)]
@@ -45,45 +40,24 @@ options = [ Option "v" ["version"]
               "Add command to run on file event."
           , Option "p" ["pattern"]
               (ReqArg (\e opts -> opts { optPatterns = optPatterns opts ++ [e] }) "pattern")
-              "Add pattern to match on file path." 
+              "Add pattern to match on file path."
           , Option "d" ["directory"]
               (ReqArg (\d opts -> opts { optDirectory = decodeString d }) "directory")
               "Set directory to watch for changes (default is ./)."
-          , Option "b" ["daemon"]
-              (ReqArg (\s opts -> opts { optIsDaemon = True, optDaemonCmd = s }) "start|stop")
-              "Attempt to run in the background as a daemon. When a previous daemon is running from your working directory this option will kill that process."
           ]
-                         
+
 header :: String
 header = "Usage: sos [vb] -c command -p pattern"
 
 version :: String
-version = "\nSteel Overseer 1.0.0.0\n"
+version = "\nSteel Overseer 1.1.0.0\n"
 
 startWithOpts :: Options -> IO ()
 startWithOpts opts = do
     let Options{..} = opts
         haveOptions   = not (null optPatterns)
         patternsValid = and $ fmap (not . null) optPatterns
-        daemonValid   = optDaemonCmd == "start" || optDaemonCmd == "stop"
     when optShowVersion $ putStrLn version
     unless patternsValid $ error "One or more patterns are empty."
-    unless daemonValid $ error "The argument to -b,--daemon must be one of start or stop."
-    didKill <- if optIsDaemon && optDaemonCmd == "stop"
-                 then do shouldKillPrev <- isRunning pIdFile
-                         if shouldKillPrev
-                           then do putStrLn "Killing previous steeloverseer daemon..."
-                                   killAndWait pIdFile 
-                                   putStrLn "Done."
-                           else putStrLn "There is no previous daemon to kill."
-                         return shouldKillPrev
-                 else return False
-    let runSteelOverseer = steelOverseer optDirectory optCommands optPatterns optIsDaemon
-    when (haveOptions && patternsValid && daemonValid) $ 
-        if not optIsDaemon 
-          then runSteelOverseer 
-          else unless didKill $ do
-                   putStrLn "Running in the background as a daemon, logging output to sos.log."
-                   runDetached (Just pIdFile) (ToFile logFile) runSteelOverseer
-
-
+    let runSteelOverseer = steelOverseer optDirectory optCommands optPatterns
+    when (haveOptions && patternsValid) runSteelOverseer
--- src/SOS.hs
+++ src/SOS.hs
@@ -6,8 +6,6 @@ import           ANSIColors
 import           System.FSNotify
 import           System.Process
 import           System.Exit
-import           System.Posix.Signals
-import           System.Posix.Daemon
 import           Control.Monad
 import           Control.Concurrent
 import           Data.Maybe
@@ -15,53 +13,38 @@ import           Text.Regex.TDFA
 import           Filesystem.Path.CurrentOS as OS
 import           Prelude hiding   ( FilePath )
 import qualified Data.Text as T
-import qualified Prelude
 
--- | A structure to hold our changed file events, 
--- list of commands to run and possibly the currently running process, 
+-- | A structure to hold our changed file events,
+-- list of commands to run and possibly the currently running process,
 -- in case it's one that hasn't terminated.
-data SOSState = SOSIdle 
+data SOSState = SOSIdle
               | SOSPending { accumulatedEvents :: [Event] }
               | SOSRunning { runningProccess   :: ProcessHandle
                            , pendingCommands   :: [String]
                            }
 
-pIdFile :: Prelude.FilePath
-pIdFile = "sos.pid"
-
-logFile :: Prelude.FilePath
-logFile = "sos.log"
-
-steelOverseer :: FilePath -> [String] -> [String] -> Bool -> IO ()
-steelOverseer dir cmds exts isDaemon = do
-    unless isDaemon $ putStrLn "Hit enter to quit.\n" 
+-- | Starts sos in a `dir` watching `ptns` to execute `cmds`.
+steelOverseer :: FilePath -> [String] -> [String] -> IO ()
+steelOverseer dir cmds ptns = do
+    putStrLn "Hit enter to quit.\n"
     wm <- startManager
     mvar <- newEmptyMVar
 
-    let predicate = actionPredicateForRegexes exts
+    let predicate = actionPredicateForRegexes ptns
         action    = performCommand mvar cmds
-    watchTree wm dir predicate action
+    _ <- watchTree wm dir predicate action
+    _ <- getLine
+    cleanup wm mvar
 
-    unless isDaemon $ do 
-        _ <- getLine
-        cleanup wm mvar
-        
-    when isDaemon $ do
-        -- Install a sigkill handler to cleanup when the daemon is killed.
-        _ <- installHandler sigQUIT (Catch $ cleanup wm mvar) Nothing 
-        forever $ threadDelay 1000000 
-
+-- | Cleans up sos on close.
 cleanup :: WatchManager -> MVar SOSState -> IO ()
 cleanup wm mvar = do
     putStrLn "Cleaning up."
     mState <- tryTakeMVar mvar
     case mState of
-        Just (SOSRunning pid _) -> terminatePID pid 
+        Just (SOSRunning pid _) -> terminatePID pid
         _ -> return ()
     stopManager wm
-
-    isDaemon <- isRunning pIdFile 
-    when isDaemon $ brutalKill pIdFile
 
 actionPredicateForRegexes :: [String] -> Event -> Bool
 actionPredicateForRegexes ptns event = or (fmap (filepath =~) ptns :: [Bool])
@@ -69,19 +52,19 @@ actionPredicateForRegexes ptns event = or (fmap (filepath =~) ptns :: [Bool])
                          Left f  -> T.unpack f
                          Right f -> T.unpack f
 
-actionPredicateForExts :: [String] -> Event -> Bool 
+actionPredicateForExts :: [String] -> Event -> Bool
 actionPredicateForExts exts event = let maybeExt = extension $ eventPath event in
     case fmap T.unpack maybeExt of
         Just ext -> ext `elem` exts
-        Nothing  -> False 
+        Nothing  -> False
 
 performCommand :: MVar SOSState -> [String] -> Event -> IO ()
 performCommand mvar cmds event = do
     cyanPrint event
     mSosState <- tryTakeMVar mvar
     sosState  <- case mSosState of
-        Nothing -> do 
-            -- This is the first file to change and there is no running process. 
+        Nothing -> do
+            -- This is the first file to change and there is no running process.
             startWriteProcess mvar cmds 1000000
             return $ SOSPending [event]
 
@@ -93,14 +76,14 @@ performCommand mvar cmds event = do
             return $ SOSPending [event]
 
         Just (SOSPending events) ->
-            -- SOS is waiting the 1sec delay for events before running the process. 
+            -- SOS is waiting the 1sec delay for events before running the process.
             return $ SOSPending $ event:events
 
         Just state -> return state
 
     putMVar mvar sosState
 
-startWriteProcess :: MVar SOSState -> [String] -> Int -> IO () 
+startWriteProcess :: MVar SOSState -> [String] -> Int -> IO ()
 startWriteProcess mvar [] _ = do
     _ <- tryTakeMVar mvar
     return ()
@@ -113,11 +96,11 @@ startWriteProcess mvar (cmd:cmds) delay = void $ forkIO $ do
     case mEvProcCurrent of
         -- This shouldn't ever happen.
         Nothing -> putMVar mvar $ SOSRunning pId cmds
-        
+
         Just _ -> void $ forkIO $ do
-            putMVar mvar $ SOSRunning pId cmds 
+            putMVar mvar $ SOSRunning pId cmds
             exitCode <- waitForProcess pId
-            case exitCode of 
+            case exitCode of
                 ExitSuccess -> do
                     greenPrint exitCode
                     startWriteProcess mvar cmds 0
@@ -125,6 +108,6 @@ startWriteProcess mvar (cmd:cmds) delay = void $ forkIO $ do
 
 terminatePID :: ProcessHandle -> IO ()
 terminatePID pid = do
-    terminateProcess pid 
+    terminateProcess pid
     putStrLn $ colorString ANSIRed "Terminated hanging process."
 
