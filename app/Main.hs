0fad75473d69b9d2ce73da5b69e9f4885cc0abaa
Mitchell Rosen <mitchellwrosen@gmail.com>
2015-12-04 18:26:44 -0800

    refactoring

--- app/Main.hs
+++ app/Main.hs
@@ -6,35 +6,26 @@
 
 module Main where
 
-import Command
+import Job
+import Rule
 import Sos
+import Template
 
-import Control.Concurrent.Async
-import Control.Concurrent.STM
-import Control.Exception
 import Control.Monad
-import Data.ByteString           (ByteString)
+import Data.ByteString     (ByteString)
 import Data.Function
-import Data.List.NonEmpty        (NonEmpty(..))
 import Data.Monoid
-import Data.Sequence             (Seq, ViewL(..), (|>), viewl)
-import Data.Yaml                 (decodeFileEither, prettyPrintParseException)
-import Lens.Micro
-import Prelude                   hiding (FilePath)
+import Data.Yaml           (decodeFileEither, prettyPrintParseException)
+import Prelude             hiding (FilePath)
 import Options.Applicative
-import System.Console.ANSI
 import System.Directory
 import System.Exit
 import System.FilePath
 import System.FSNotify
-import System.IO
-import System.Process
-import Text.Printf
 import Text.Regex.TDFA
 
 import qualified Data.ByteString.Char8 as BS
 import qualified Data.List.NonEmpty    as NE
-import qualified Data.Sequence         as Seq
 
 version :: String
 version = "Steel Overseer 2.0"
@@ -73,201 +64,70 @@ main = execParser opts >>= main'
 
 main' :: Options -> IO ()
 main' Options{..} = do
-    -- Parse .sosrc command plans.
-    rc_plans <- parseSosrc
+    -- Parse .sosrc rules.
+    rc_rules <- parseSosrc
 
-    -- Parse cli commands, where one command is created per pattern that
-    -- executes each of the specified sub-commands sequentially.
-    cli_plans <- do
+    -- Parse cli rules, where one rule is created per pattern that executes each
+    -- of commands sequentially.
+    cli_rules <- do
         let patterns =
-                case (rc_plans, optPatterns) of
+                case (rc_rules, optPatterns) of
                     -- If there are no commands in .sosrc, and no patterns
                     -- specified on the command line, default to ".*"
                     ([], []) -> [".*"]
                     _ -> optPatterns
-        runSos (mapM (\pattern -> buildCommandPlan pattern optCommands) patterns)
+        runSos (mapM (\pattern -> buildRule pattern optCommands) patterns)
 
-    (target, plans) <- do
+    (target, rules) <- do
         is_dir  <- doesDirectoryExist optTarget
         is_file <- doesFileExist optTarget
         case (is_dir, is_file) of
-            (True, _) -> pure (optTarget, cli_plans ++ rc_plans)
+            (True, _) -> pure (optTarget, cli_rules ++ rc_rules)
             -- If the target is a single file, completely ignore the .sosrc
             -- commands and the cli commands.
             (_, True) -> do
-                plan <- runSos (buildCommandPlan (BS.pack optTarget) optCommands)
-                pure (takeDirectory optTarget, [plan])
+                rule <- runSos (buildRule (BS.pack optTarget) optCommands)
+                pure (takeDirectory optTarget, [rule])
             _ -> do
                 putStrLn ("Target " ++ optTarget ++ " is not a file or directory.")
                 exitFailure
 
-    -- Queue of commands to run. The next list of commands to run is at the head
-    -- of the list.
-    cmds_queue_tvar <- newTVarIO Seq.empty
-
-    cwd <- getCurrentDirectory
-
     putStrLn "Hit Ctrl+C to quit."
+
+    withManager $ \wm -> do
+        job_queue <- newJobQueue
+        spawnFileWatcherThread wm job_queue target rules
+        forever (dequeueJob job_queue >>= runJob job_queue)
+
+spawnFileWatcherThread :: WatchManager -> JobQueue -> FilePath -> [Rule] -> IO ()
+spawnFileWatcherThread wm job_queue target rules = do
+    cwd <- getCurrentDirectory
 
     let eventRelPath :: Event -> FilePath
         eventRelPath event = makeRelative cwd (eventPath event)
 
         predicate :: Event -> Bool
-        predicate event = any (\plan -> match (cmdRegex plan) (eventRelPath event)) plans
+        predicate event = any (\rule -> match (ruleRegex rule) (eventRelPath event)) rules
 
-    withManager $ \wm -> do
-        _ <- watchTree wm target predicate $ \event -> do
-            -- putStrLn ("\n" <> colored Cyan (showEvent event cwd))
-            let path = BS.pack (eventRelPath event)
+    _ <- watchTree wm target predicate $ \event -> do
+        let path = BS.pack (eventRelPath event)
 
-            commands <- concat <$> mapM (maybeInstantiateTemplate path) plans
-            when (commands /= [])
-                (atomically (enqueueCommands cmds_queue_tvar (showEvent event cwd) (NE.fromList commands)))
+        commands <- concat <$> mapM (instantiateTemplates path) rules
+        when (commands /= [])
+            (enqueueJob (showEvent event cwd) (NE.fromList commands) job_queue)
 
-        _ <- forever $ do
-            (event, cmds, tmvar) <- atomically $ do
-                cmds_queue <- readTVar cmds_queue_tvar
-                case viewl cmds_queue of
-                    EmptyL -> retry
-                    ((event0, cmds0, tmvar0) :< _) -> do
-                        -- Clear the TMVar which might have been set before we've even
-                        -- started running the commands.
-                        _ <- tryTakeTMVar tmvar0
-                        pure (event0, cmds0, tmvar0)
-
-            -- Spawn a thread to run the commands, then wait on either that run to
-            -- finish *or* the same original TMVar to be signaled (so, a particular
-            -- pipeline of commands can only "interrupt" itself - all other concurrently
-            -- scheduled commands are simply enqueued.
-            let loop = do
-                    putStrLn ("\n" <> colored Cyan event)
-                    a <- async (runCommands (NE.toList cmds))
-
-                    result <- atomically $
-                        (pollSTM a >>= \case
-                            Nothing -> retry
-                            Just r -> do
-                                -- Partial pattern matching is safe here because we're the
-                                -- only thread that removes from the command queue, and we
-                                -- just read a non-empty sequence from it above.
-                                modifyTVar' cmds_queue_tvar
-                                    (\s -> case viewl s of
-                                               (_ :< rest) -> rest)
-                                pure (Left r))
-                        <|>
-                        Right <$> takeTMVar tmvar
-
-                    case result of
-                        -- Commands threw an exception - a ThreadKilled we are
-                        -- okay with, but anything else we should print to the
-                        -- console.
-                        Left (Left ex) ->
-                            case fromException ex of
-                                Just ThreadKilled -> pure ()
-                                _ -> putStrLn (colored Red ("Exception: " ++ show ex))
-
-                        -- Commands finished successfully - go back to outer loop.
-                        Left (Right True) -> pure ()
-
-                        -- A command failed - if there is another enqueued list
-                        -- of commands to run, prompt the user to continue or
-                        -- abort.
-                        Left (Right False) -> do
-                            cmds_queue <- atomically (readTVar cmds_queue_tvar)
-                            case length cmds_queue of
-                                0 -> pure ()
-                                n -> do
-                                    putStrLn (colored Yellow (show n ++ " job(s) still pending."))
-
-                                    hSetBuffering stdin NoBuffering
-                                    continue <- fix (\prompt -> do
-                                        putStr (colored Yellow "Press 'c' to continue, 'p' to print, or 'a' to abort: ")
-                                        hFlush stdout
-                                        (getChar <* putStrLn "") >>= \case
-                                            'c' -> pure True
-                                            'a' -> pure False
-                                            'p' -> do
-                                                mapM_ (\(c:|cs) -> do
-                                                          putStrLn ("- " ++ c)
-                                                          mapM_ (putStrLn . ("  " ++)) cs)
-                                                      (cmds_queue^..traversed._2)
-                                                prompt
-                                            _ -> prompt)
-                                    hSetBuffering stdin LineBuffering
-
-                                    -- Here, it's possible we've reported that
-                                    -- `n` jobs were enqueued, but by the time
-                                    -- the user decides to abort them, more
-                                    -- were added. Oh well, delete them all.
-                                    unless continue $ do
-                                        n' <- atomically (length <$>swapTVar cmds_queue_tvar mempty)
-                                        putStrLn (colored Red ("Aborted " ++ show n' ++ " job(s)."))
-
-
-                        -- Another filesystem event triggered this list of commands
-                        -- to be run again - cancel the previous run and start over.
-                        Right () -> do
-                            cancel a
-                            _ <- waitCatch a
-                            loop
-            loop
-
-        pure ()
-
-maybeInstantiateTemplate :: ByteString -> CommandPlan -> IO [Command]
-maybeInstantiateTemplate path CommandPlan{..} =
-    case match cmdRegex path of
-        [] -> pure []
-        (captures:_) -> runSos (mapM (instantiateTemplate captures) cmdTemplates)
-
-type EnqueuedCommands = (String, NonEmpty Command, TMVar ())
-type CommandsQueue = Seq EnqueuedCommands
-
--- | Walk the commands queue - if we see we've already been enqueued, signal the
--- corresponding TMVar. Otherwise, enqueue us at the end.
-enqueueCommands :: TVar CommandsQueue -> String -> NonEmpty Command -> STM ()
-enqueueCommands cmds_queue_tvar event commands =
-    let loop :: CommandsQueue -> STM ()
-        loop cmds_queue =
-            case viewl cmds_queue of
-                EmptyL -> do
-                    tmvar <- newEmptyTMVar
-                    modifyTVar' cmds_queue_tvar (\s -> s |> (event, commands, tmvar))
-
-                (_, commands', tmvar) :< cmds_queue_tail ->
-                    if commands == commands'
-                        then void (tryPutTMVar tmvar ())
-                        else loop cmds_queue_tail
-
-    in readTVar cmds_queue_tvar >>= loop
-
--- Run a list of shell commands sequentially. If a command returns ExitFailure,
--- don't run the rest. Return whether or not all commands completed
--- successfully.
-runCommands :: [Command] -> IO Bool
-runCommands cmds0 = go 1 cmds0
+    pure ()
   where
-    go :: Int -> [Command] -> IO Bool
-    go _ [] = pure True
-    go n (cmd:cmds) = do
-        putStrLn (colored Magenta (printf "[%d/%d] " n (length cmds0)) <> cmd)
-
-        let create = (\(_, _, _, ph) -> ph) <$> createProcess (shell cmd)
-            teardown ph = do
-                putStrLn (colored Yellow ("Canceling: " ++ cmd))
-                terminateProcess ph
-        bracketOnError create teardown waitForProcess >>= \case
-            ExitSuccess -> do
-                putStrLn (colored Green "Success ✓")
-                go (n+1) cmds
-            _ -> do
-                putStrLn (colored Red "Failure ✗")
-                pure False
+    instantiateTemplates :: ByteString -> Rule -> IO [ShellCommand]
+    instantiateTemplates path Rule{..} =
+        case match ruleRegex path of
+            [] -> pure []
+            (captures:_) -> runSos (mapM (instantiateTemplate captures) ruleTemplates)
 
 --------------------------------------------------------------------------------
 
--- Parse a list of command plans from .sosrc.
-parseSosrc :: IO [CommandPlan]
+-- Parse a list of rules from .sosrc.
+parseSosrc :: IO [Rule]
 parseSosrc = do
     exists <- doesFileExist ".sosrc"
     if exists
@@ -276,22 +136,15 @@ parseSosrc = do
                 Left err -> do
                     putStrLn ("Error parsing .sosrc:\n" ++ prettyPrintParseException err)
                     exitFailure
-                Right (raw_plans :: [RawCommandPlan]) -> do
-                    plans <- runSos (mapM buildRawCommandPlan raw_plans)
-                    putStrLn "Found .sosrc"
-                    pure (concat plans)
+                Right (raw_rules :: [RawRule]) -> do
+                    rules <- runSos (mapM buildRawRule raw_rules)
+                    putStrLn (case length raw_rules of
+                                  1 -> "Found 1 rule in .sosrc"
+                                  n -> "Found " ++ show n ++ " rules in .sosrc")
+                    pure (concat rules)
         else pure []
 
 --------------------------------------------------------------------------------
-
-colored :: Color -> String -> String
-colored c s = color c <> s <> reset
-
-color :: Color -> String
-color c = setSGRCode [SetColor Foreground Dull c]
-
-reset :: String
-reset = setSGRCode [Reset]
 
 showEvent :: Event -> FilePath -> String
 showEvent (Added fp _)    cwd = "Added: "    ++ makeRelative cwd fp
